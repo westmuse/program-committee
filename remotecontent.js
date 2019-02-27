d9ada22baebaead1c67276040562a769cd9a2fd4
Koven J. Smith <koven.smith@blantonmuseum.org>
2019-02-27 07:49:45 -0500

    Attempt to remove Word characters from markdown files (not working)

--- remotecontent.js
+++ remotecontent.js
@@ -1,3 +1,26 @@
+var replaceWordChars = function(text) {
+    var s = text;
+    // smart single quotes and apostrophe
+    s = s.replace(/[\u2018\u2019\u201A]/g, "\'");
+    // smart double quotes
+    s = s.replace(/[\u201C\u201D\u201E]/g, "\"");
+    // ellipsis
+    s = s.replace(/\u2026/g, "...");
+    // dashes
+    s = s.replace(/[\u2013\u2014]/g, "-");
+    // circumflex
+    s = s.replace(/\u02C6/g, "^");
+    // open angle bracket
+    s = s.replace(/\u2039/g, "<");
+    // close angle bracket
+    s = s.replace(/\u203A/g, ">");
+    // spaces
+    s = s.replace(/[\u02DC\u00A0]/g, " ");
+    // linefeeds and carriage returns
+    s = s.replace(/[\r\n]*/g, "");
+    return s;
+}
+
 var fs = require('fs'); //to access filewrite
        var toMarkdown = require ('to-markdown'); //to convert HTML to Markdown
        var request = require("request"); //to request the JSON file from the server
@@ -10,6 +33,9 @@ var fs = require('fs'); //to access filewrite
       url: 'https://gist.github.com/5easypieces/4ce57c27bbbfe7728c81fb6548801b0e/raw/',
 
      };
+
+
+
 
    //Request
    request(options, function (error, response, body) {
@@ -24,7 +50,20 @@ var fs = require('fs'); //to access filewrite
       console.log(obj.Proposal.length + " total submissions");
       for (var j=0; j<obj.Proposal.length; j++) {
 
-        myObj = obj.Proposal[j];
+
+          var myObj = obj.Proposal[j];
+          for (var x=0; x<myObj.length; x++) {
+            var htmlstring = myObj[x]; //The content that goes into the Markdown part
+            if (typeof htmlstring === 'string' || htmlstring instanceof String) {
+              var markdowntext = toMarkdown(htmlstring); //The HTML conversion
+              console.log("converted!");
+            } else {
+              var markdowntext = htmlstring;
+              console.log("didn't convert");
+            }
+          }
+
+
 
 
 
@@ -40,7 +79,9 @@ var fs = require('fs'); //to access filewrite
 
           // var toFrontmatter = myObj[x];
         //   var toml = tomlify(myObj, {delims: true}, {indent: true});
-          var toml = tomlify.toToml(myObj, {space: 2});
+
+
+          var toml = tomlify.toToml(markdowntext, {space: 2});
 
 
           var full_proposal = '+++\n' + toml + '\n+++\n';
